2bd501d6e8a9348a99bf2ab39bff8849542a7e04
Seth Lessard <seth.lessard@pm.me>
2019-11-30 13:21:56 -0600

    Remove build

--- build/renderer/old/actions/user.js
+++
@@ -1,7 +0,0 @@
-// import { createAction } from 'redux-actions';
-// export default {
-//   login: createAction('USER_LOGIN'),
-//   logout: createAction('USER_LOGOUT'),
-// };
-"use strict";
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInJlbmRlcmVyL29sZC9hY3Rpb25zL3VzZXIuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFFQTtBQUNBO0FBQ0E7QUFDQSIsInNvdXJjZXNDb250ZW50IjpbIi8vIGltcG9ydCB7IGNyZWF0ZUFjdGlvbiB9IGZyb20gJ3JlZHV4LWFjdGlvbnMnO1xuXG4vLyBleHBvcnQgZGVmYXVsdCB7XG4vLyAgIGxvZ2luOiBjcmVhdGVBY3Rpb24oJ1VTRVJfTE9HSU4nKSxcbi8vICAgbG9nb3V0OiBjcmVhdGVBY3Rpb24oJ1VTRVJfTE9HT1VUJyksXG4vLyB9O1xuIl0sImZpbGUiOiJyZW5kZXJlci9vbGQvYWN0aW9ucy91c2VyLmpzIn0=
