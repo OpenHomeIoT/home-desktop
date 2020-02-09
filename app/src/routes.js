693804f4f840ffd08487f4c14c044d88e9c591e5
Seth Lessard <seth.lessard@pm.me>
2020-02-09 13:22:29 -0600

    Add Services

--- app/src/routes.js
+++ app/src/routes.js
@@ -1,10 +1,17 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, useParams } from "react-router-dom";
 
 import AutomationsView from "./views/AutomationsView";
 import HomeView from "./views/HomeView";
 import RoutinesView from "./views/RoutinesView";
 import SettingsView from "./views/SettingsView";
+import ServicesView from "./views/service/ServicesView";
+import ServiceView from "./views/service/ServiceView";
+
+const ServiceViewWrapper = () => {
+  const { id } = useParams();
+  return <ServiceView id={id} />;
+}
 
 export default (
   <Switch>
@@ -14,12 +21,14 @@ export default (
     <Route path="/automate">
       <AutomationsView />
     </Route>
-    <Route path="/devices">
-      {/* TODO: implement */}
-      Devices
-    </Route>
     <Route path="/routines">
       <RoutinesView />
+    </Route>
+    <Route path="/services/:id">
+      <ServiceViewWrapper />
+    </Route>
+    <Route exact path="/services">
+      <ServicesView />
     </Route>
     <Route path="/settings">
       <SettingsView />
