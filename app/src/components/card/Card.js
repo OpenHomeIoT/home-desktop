aa7173968ec20688a43ccda50164b1dea0ac8f39
Seth Lessard <seth.lessard@pm.me>
2020-02-09 13:24:22 -0600

    Moving styles to .css

--- app/src/components/card/Card.js
+++ app/src/components/card/Card.js
@@ -1,30 +1,27 @@
 import React from "react";
 import PropTypes from "prop-types";
-
 import Paper from "../Paper";
+import "./Card.css";
 
-const Card = (props) => {
+const Card = ({ children, style: compStyle, z=1 }) => {
     const style = {
-        card: {
-            boxShadow: "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)"
+        card: {},
+        paper: {
+            backgroundColor: (compStyle) ? compStyle.backgroundColor : "auto",
+            borderRadius: 4
         }
     };
 
-    if (props.z === 2) {
-        style.card.boxShadow = "0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)";
-    } else if (props.z === 3) {
-        style.card.boxShadow = "0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)";
-    } else if (props.z === 4) {
-        style.card.boxShadow = "0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)";
-    } else if (props.z === 5) {
-        style.card.boxShadow = "0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22)";
-    }
-
-    Object.assign(style.card, props.style);
+    if (z < 1) z = 1;
+    if (z > 5) z = 5;
+    const className = `Card Card-${z}`;
+    Object.assign(style.card, compStyle);
     return (
-        <Paper style={style.card}>
-            {props.children}
-        </Paper>
+        <div className={className} style={style.card}>
+            <Paper style={style.paper}>
+                {children}
+            </Paper>
+        </div>
     );
 };
 
